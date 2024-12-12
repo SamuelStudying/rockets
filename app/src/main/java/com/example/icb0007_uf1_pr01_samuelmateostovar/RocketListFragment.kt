ad1151e4dcf997e3d0d8560e9df031248c9a2ac9
Samuel Mateos <sfakyu96@gmail.com>
2024-12-12 21:58:58 +0100

    Eliminación de librerias sin usar

--- app/src/main/java/com/example/icb0007_uf1_pr01_samuelmateostovar/RocketListFragment.kt
+++ app/src/main/java/com/example/icb0007_uf1_pr01_samuelmateostovar/RocketListFragment.kt
@@ -1,7 +1,6 @@
 package com.example.icb0007_uf1_pr01_samuelmateostovar
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,10 +10,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.icb0007_uf1_pr01_samuelmateostovar.adapter.RocketAdapter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class RocketListFragment : Fragment() {
 
